5cdac1c87379695befd6bd8b146c974ff2fd4ddd
Danijel Pavić <danijelpavic21@gmail.com>
2017-02-08 00:03:56 +0100

    Added controls.

--- DBZ_Player/DBZ_Player/Form1.Designer.cs
+++ DBZ_Player/DBZ_Player/Form1.Designer.cs
@@ -28,20 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.wmp = new AxWMPLib.AxWindowsMediaPlayer();
+            this.Playlist = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.wmp)).BeginInit();
             this.SuspendLayout();
+            // 
+            // wmp
+            // 
+            this.wmp.Enabled = true;
+            this.wmp.Location = new System.Drawing.Point(-4, 0);
+            this.wmp.Name = "wmp";
+            this.wmp.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("wmp.OcxState")));
+            this.wmp.Size = new System.Drawing.Size(301, 45);
+            this.wmp.TabIndex = 0;
+            // 
+            // Playlist
+            // 
+            this.Playlist.FormattingEnabled = true;
+            this.Playlist.Location = new System.Drawing.Point(-4, 86);
+            this.Playlist.Name = "Playlist";
+            this.Playlist.Size = new System.Drawing.Size(301, 121);
+            this.Playlist.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(126, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Playlist";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(-4, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(301, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Import Songs";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(297, 205);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Playlist);
+            this.Controls.Add(this.wmp);
             this.Name = "Form1";
             this.Text = "DBZ Player";
+            ((System.ComponentModel.ISupportInitialize)(this.wmp)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer wmp;
+        private System.Windows.Forms.ListBox Playlist;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- DBZ_Player/DBZ_Player/Form1.cs
+++ DBZ_Player/DBZ_Player/Form1.cs
@@ -16,5 +16,10 @@ namespace DBZ_Player
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
