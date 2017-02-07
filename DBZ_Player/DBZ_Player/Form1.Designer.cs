6af758655fd37716c2cfd7105b5a61a6d591b8db
Danijel Pavić <danijelpavic21@gmail.com>
2017-02-08 00:28:32 +0100

    Fixed window and controls scaling with TableLayoutPanel.

--- DBZ_Player/DBZ_Player/Form1.Designer.cs
+++ DBZ_Player/DBZ_Player/Form1.Designer.cs
@@ -31,45 +31,38 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.wmp = new AxWMPLib.AxWindowsMediaPlayer();
             this.Playlist = new System.Windows.Forms.ListBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.TableLayout = new System.Windows.Forms.TableLayoutPanel();
             ((System.ComponentModel.ISupportInitialize)(this.wmp)).BeginInit();
+            this.TableLayout.SuspendLayout();
             this.SuspendLayout();
             // 
             // wmp
             // 
+            this.wmp.Dock = System.Windows.Forms.DockStyle.Fill;
             this.wmp.Enabled = true;
-            this.wmp.Location = new System.Drawing.Point(-4, 0);
+            this.wmp.Location = new System.Drawing.Point(3, 3);
             this.wmp.Name = "wmp";
             this.wmp.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("wmp.OcxState")));
-            this.wmp.Size = new System.Drawing.Size(301, 45);
+            this.wmp.Size = new System.Drawing.Size(337, 125);
             this.wmp.TabIndex = 0;
             // 
             // Playlist
             // 
+            this.Playlist.Dock = System.Windows.Forms.DockStyle.Fill;
             this.Playlist.FormattingEnabled = true;
-            this.Playlist.Location = new System.Drawing.Point(-4, 86);
+            this.Playlist.Location = new System.Drawing.Point(3, 163);
             this.Playlist.Name = "Playlist";
-            this.Playlist.Size = new System.Drawing.Size(301, 121);
+            this.Playlist.Size = new System.Drawing.Size(337, 141);
             this.Playlist.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Location = new System.Drawing.Point(126, 86);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(39, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Playlist";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(-4, 51);
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(3, 134);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(301, 23);
+            this.button1.Size = new System.Drawing.Size(337, 23);
             this.button1.TabIndex = 3;
             this.button1.Text = "Import Songs";
             this.button1.UseVisualStyleBackColor = true;
@@ -77,22 +70,38 @@
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "MP3 Files|*.mp3";
             this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.Title = "MP3 Player";
+            // 
+            // TableLayout
+            // 
+            this.TableLayout.ColumnCount = 1;
+            this.TableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.TableLayout.Controls.Add(this.wmp, 0, 0);
+            this.TableLayout.Controls.Add(this.button1, 0, 1);
+            this.TableLayout.Controls.Add(this.Playlist, 0, 2);
+            this.TableLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TableLayout.Location = new System.Drawing.Point(0, 0);
+            this.TableLayout.Name = "TableLayout";
+            this.TableLayout.RowCount = 3;
+            this.TableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 42.85714F));
+            this.TableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.523809F));
+            this.TableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 47.61905F));
+            this.TableLayout.Size = new System.Drawing.Size(343, 307);
+            this.TableLayout.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(297, 205);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.Playlist);
-            this.Controls.Add(this.wmp);
+            this.ClientSize = new System.Drawing.Size(343, 307);
+            this.Controls.Add(this.TableLayout);
             this.Name = "Form1";
             this.Text = "DBZ Player";
             ((System.ComponentModel.ISupportInitialize)(this.wmp)).EndInit();
+            this.TableLayout.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -100,9 +109,9 @@
 
         private AxWMPLib.AxWindowsMediaPlayer wmp;
         private System.Windows.Forms.ListBox Playlist;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TableLayoutPanel TableLayout;
     }
 }
 
